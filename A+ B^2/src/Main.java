f0698f8267fb3e34422802ce55e55595fcda4037
AnonymousAmigo32 <AnonymousAmigo32@users.noreply.github.com>
2017-10-22 19:12:16 +0530

    A + B^2

---
+++ A+ B^2/src/Main.java
@@ -0,0 +1,15 @@
+import mooc.EdxIO;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		
+		try(EdxIO io = EdxIO.create();){
+			
+			int A = io.nextInt(), B = io.nextInt();
+			
+			io.print(A+(B*B));
+		}
+	}		
+}
